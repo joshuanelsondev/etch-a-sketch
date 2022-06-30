69dfdf74ae5efeb91f96805b5641e68c98e97b52
Joshua Nelson <joshuanelson894@gmail.com>
2022-06-29 22:28:56 -0400

    Create a function 'createGrid()
    the function works with the range slider to create different size grids

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,7 +1,26 @@
+const container = document.getElementById('container');
 const slider = document.getElementById('slide');
 const sliderValueDisplay = document.getElementById('sliderValueDisplay');
+let gridSize;
 sliderValueDisplay.innerHTML = slider.value;
 
 slider.oninput = function() {
     sliderValueDisplay.textContent = this.value;
+    gridSize = this.value;
+    createGrid();
+    
 };
+
+function createGrid() {
+    container.replaceChildren();
+    for (let i = 0; i < gridSize; i++) {
+        const row = container.appendChild(document.createElement('div'));
+        for (let j = 0; j < gridSize; j++) {
+            const column = document.createElement('div');
+            column.classList.add('gridBox');
+            container.appendChild(column);
+        }
+    }
+    
+}
+
