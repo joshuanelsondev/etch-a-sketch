6073f0cf73c66a5309a9833de42afcbc9a69258f
Joshua Nelson <joshuanelson894@gmail.com>
2022-07-01 16:26:59 -0400

    Create grid lines
    style the knobs and sketch container, have objects overlap onto the container

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -4,6 +4,7 @@ const sliderValueDisplay = document.getElementById('sliderValueDisplay');
 let gridSize = 5;
 sliderValueDisplay.innerHTML = slider.value;
 
+// User range slider for creating the grid size
 slider.oninput = function() {
     sliderValueDisplay.textContent = this.value;
     gridSize = this.value;
@@ -14,11 +15,14 @@ slider.oninput = function() {
 function createGrid() {
     screen.replaceChildren();
     for (let i = 0; i < gridSize; i++) {
-        const row = screen.appendChild(document.createElement('div'));
+        // const row = screen.appendChild(document.createElement('div'));
         for (let j = 0; j < gridSize; j++) {
             const column = document.createElement('div');
             column.classList.add('gridBox');
+            screen.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+            screen.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
             screen.appendChild(column);
+
         }
     }
 }
