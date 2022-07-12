6e0005586094845ff2375dccddb07d48066378a1
Joshua Nelson <joshuanelson894@gmail.com>
2022-07-12 15:53:24 -0400

    Create functions to add a click and draw effect
    move screen.addEventListener to bottom of page
    
    create activatePen() to assign element to activePen variable
    
    edit currentPen to use the activePen assignment to determine the background color of the grid cells
    
    edit toggleGridPen() to use currentPen() and toggle activeGrid

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -6,9 +6,16 @@ const rainbow = document.getElementById('rainbow');
 const colorShader = document.getElementById('shader');
 const clearScreen = document.getElementById('clear');
 const colorPicker = document.getElementById('pickColor');
+const colorButton = document.getElementsByClassName('btn');
 let gridSize = 25;
+let gridElement = [];
 let divs = screen.getElementsByTagName('div');
-let currentPen = () => defaultPen();
+let activeGrid = false;
+let activePen = 'default';
+let userPenColor;
+
+
+
 sliderValueDisplay.innerHTML = slider.value;
 
 // User range slider for creating the grid size
@@ -23,51 +30,75 @@ slider.oninput = function() {
 
 function createGrid() {
     screen.replaceChildren();
-    
-    for (let i = 0; i < gridSize; i++) {
-        // const row = screen.appendChild(document.createElement('div'));
-        for (let j = 0; j < gridSize; j++) {
-            const column = document.createElement('div');
-            column.classList.add('gridBox');
-            screen.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-            screen.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-            screen.appendChild(column);
-            
-           
-            
-            
-
-        }
+    screen.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    screen.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        gridElement[i] = document.createElement('div');
+        gridElement[i].classList.add('gridBox');
+        gridElement[i].addEventListener('click', currentPen);
+        screen.appendChild(gridElement[i]);
         
+    }  
+    activeGrid = false; 
+}
+
+function toggleGridPen(){
+    if(!activeGrid) {
+        gridElement.forEach(cell => {
+            cell.addEventListener('mouseover', currentPen);
+        });
+        activeGrid = true;
+    } else {
+        gridElement.forEach(cell => {
+            cell.removeEventListener('mouseover', currentPen);
+        });
+        activeGrid = false;
     }
-    currentPen();
+}
+
+
+function currentPen(e) {
+    
+    switch(activePen) {
+        case 'pickColor':
+            e.target.style.backgroundColor = colorPicker.value;
+        break;
+        case 'black':
+            e.target.style.backgroundColor = 'black';
+        break;
+        case 'rainbow':
+            e.target.style.backgroundColor = 'pink'; //Create a function for rainbow colors
+        break;
+        case 'shader':
+            e.target.style.backgroundColor = 'gray'; //Create a function that shades the color
+        break;
+        default:    
+            e.target.style.backgroundColor = 'black';
+    }
+    
     
 }
 
-// Button functions to allow user to choose pen color or clear the grid
-function defaultPen(){
-    for (let i = 0; i < divs.length; i++) {
-        divs[i].addEventListener('mouseover', function() {this.style.backgroundColor = 'black'; });
-       
-    }
-    
-}
-
-function userPen(){
-    
-    for (let i = 0; i < divs.length; i++) {
-        divs[i].addEventListener('mouseover', function() {this.style.backgroundColor = colorPicker.value; });
-    }
-    currentPen = userPen;
-}
 
 // Button events
-blackPen.addEventListener('click', defaultPen);
+blackPen.addEventListener('click', activatePen);
 
-colorPicker.addEventListener('click', userPen);
+colorPicker.addEventListener('click', activatePen);
 
 clearScreen.addEventListener('click', createGrid);
 
+rainbow.addEventListener('click', activatePen);
+
+colorShader.addEventListener('click', activatePen);
+
+function activatePen(e) {
+    activePen = e.target.id;
+    active = true;
+   
+}
+
+
+screen.addEventListener('click', toggleGridPen);
 
 window.onload = () => {
     createGrid();
